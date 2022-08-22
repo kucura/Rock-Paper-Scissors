a13e4a418543c0e47f9af418f44222f204c1ab42
kucura <rukicu.gorski@gmail.com>
2022-08-22 10:20:09 +0200

    GUI done

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Rock-Paper-Scissors
 TOP Project:Rock Paper Scissors 
+
+
+console.log("Final score User "+user+":"+pc+" Computer")
+
+if (user>pc) {
+  console.log("YOU WIN!!!")
+  } else if (user<pc) {
+  console.log ("PC WINS!! :(")
+  } else {
+  console.log("It's a tie!")
+  }
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+
+
+
+let pc = 0
+let user = 0
+
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const number = Math.floor(Math.random() * choices.length);
@@ -5,21 +13,58 @@ function getComputerChoice() {
     return computer
   
   }
-  const playerEnter = prompt("Enter rock, paper, or scissors:")
-  const playerSelection = playerEnter.toLowerCase()
+
   
-  console.log("Player plays: "+ playerSelection)
-  const computerSelection = getComputerChoice();
-  console.log("Computer plays: "+computerSelection)
   
   function playRound(playerSelection, computerSelection) {
+    
+    
     if (playerSelection === "rock" && computerSelection === "scissors") {
-      console.log("Rock beats scissors! Player wins!")
-    } //here
+      console.log("Rock beats scissors! Player wins!"); user++
+      } else if (playerSelection ==="rock" && computerSelection === "paper") {
+      console.log ("Paper beats rock! Computer wins!"); pc++
+      } else if (playerSelection ==="rock" && computerSelection === "rock") {
+      console.log ("It's a tie!")
+      } else if (playerSelection === "paper" && computerSelection === "scissors") {
+      console.log("Scissors cut paper! Computer wins!"); pc++
+      } else if (playerSelection === "paper" && computerSelection === "rock") {
+      console.log("Paper beats rock! Player wins!"); user++
+      } else if (playerSelection === "paper" && computerSelection === "paper") {
+      console.log ("It's a tie!")
+      } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      console.log ("Rock beats scissors! Computer wins!"); pc++
+      } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      console.log ("Scissors cut paper! Player wins!"); user++
+      } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+      console.log ("It's a tie!")
+      } else console.log("You entered wrong word!!!")
+        
+     
   
   }
   
   
   
-  playRound(playerSelection, computerSelection)
+ for (let i = 0; i < 5; i++) {
+  getComputerChoice() 
+  
+  //const playerSelection = playerEnter.toLowerCase()
+  
+  console.log("Player plays: "+ playerSelection)
+  const computerSelection = getComputerChoice();
+  console.log("Computer plays: "+computerSelection)
+   
+   playRound(playerSelection, computerSelection)
+}
+
+console.log("Final score User "+user+":"+pc+" Computer")
+
+if (user>pc) {
+  console.log("YOU WIN!!!")
+  } else if (user<pc) {
+  console.log ("PC WINS!! :(")
+  } else {
+  console.log("It's a tie!")
+  }
+  
   
